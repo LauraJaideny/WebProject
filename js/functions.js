b6e2e1f17ac4050aeb81cfa4191c6f0efb0f05cd
Adrian Peña <adrianpena_94@hotmail.com>
2018-04-21 15:58:13 -0500

    Logout, sessions terminado

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,6 @@
  $(document).ready(function() {
+
+    getSession();
           
   $("#registerButton").on("click", function(){
         var jsonObject = {
@@ -28,4 +30,55 @@
         });
    });
 
+  $("#logoutBtn").on("click", function(){
+        logout();
+  });
+
 });
+
+ function getSession(){
+    var jsonToSend = {"action" : 'GETSESSION' };
+        $.ajax({
+                url : "data/applicationLayer.php",
+                type : "POST",
+                dataType : "json",
+                data : jsonToSend,
+                ContentType : "application/json",
+                success : function(dataReceived){
+                    //$("#loginCurr").text("Currently logged in as "+dataReceived.firstname+" "+dataReceived.lastname);
+                    console.log("Currently logged in as "+dataReceived.firstname+" "+dataReceived.lastname);
+                    $("#myProfile").text(dataReceived.firstname);
+                },
+                error : function(errorMessage){
+                    //alert(errorMessage.statusText);
+                    console.log(errorMessage);
+                    console.log("No hay nadie conectado");
+                    alert("Please login to access this page");
+                    window.location.replace("login.html");
+                }
+
+            });
+}
+
+function logout(){
+        var jsonToSend = {"action" : 'LOGOUT' };
+        $.ajax({
+                url : "data/applicationLayer.php",
+                type : "POST",
+                dataType : "json",
+                data : jsonToSend,
+                ContentType : "application/json",
+                success : function(dataReceived){
+                    console.log("hola "+dataReceived);
+                    window.location.replace("index.html");
+                },
+                error : function(errorMessage){
+                    alert(errorMessage.statusText);
+                    console.log(errorMessage);
+                    console.log("Error logout");
+                }
+
+            });
+
+
+}
